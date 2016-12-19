0ceafaedde02d888821d4ef6c271c886cb51348d
aawangustc <sa612248@mail.ustc.edu.cn>
2016-12-19 22:32:25 +0800

    再推一次

---
+++ src/org/liuyun/multilabel_classification/MultiClassClassifier.java
@@ -0,0 +1,41 @@
+package org.liuyun.multilabel_classification;
+import java.io.File;
+
+import java.util.ArrayList;
+
+
+public class MultiClassClassifier {
+	private class LabelInfo
+	{
+	    public String labelName;
+	    //public String labelModelPath;
+	    public String trainDataPath;
+	}
+	public ArrayList<LabelInfo> LoadData(String dataPath)
+	{
+		ArrayList<LabelInfo> labelList = new ArrayList<LabelInfo>();
+		LabelInfo tmp = new LabelInfo();
+		File path=new File(dataPath);
+		File[] fileList = path.listFiles();
+		for (File f : fileList) {
+			tmp.trainDataPath = f.getAbsolutePath();
+			tmp.labelName = f.getName();
+			labelList.add(tmp);
+		}		
+		return labelList;		
+	}
+	public String MultiLabelTrain(String dataPath,String modelPath)
+	{
+		ArrayList<LabelInfo> labelList = LoadData(dataPath);
+		for (LabelInfo labelInfo : labelList) {
+			SingleClassClassifer sc = new SingleClassClassifer();
+			sc.train(labelInfo.trainDataPath, modelPath+"/"+labelInfo.labelName);			
+		}		
+		return modelPath;
+		
+	}
+	public static void main (String args[]) {
+		MultiClassClassifier myClassifier = new MultiClassClassifier();
+		myClassifier.MultiLabelTrain("/home/aa/文档/THUCNews/test/test", "/home/aa/文档/THUCNews/test/test");
+	}
+}
